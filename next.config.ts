cdc4005ef2d41cd0fe7206b546ed44ba541f2684
Divyom Sharma <divyomsharma180307@gmail.com>
2026-03-08 19:54:35 +0530

    fix: remove middleware.ts from vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  trailingSlash: false,
+  /* config options here */
 };
 
 export default nextConfig;
